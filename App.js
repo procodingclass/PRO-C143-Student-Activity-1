8070e5ee855aa69300da818e3cada45a8fd6babc
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-04-08 00:32:42 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import StackNav from "./navigation/stackNav";
 import { View,SafeAreaView,Platform,StatusBar } from "react-native";
+import HomeScreen from "./screens/Home";
 
 
 export default function App() {
   return(
     <View style={{flex:1}}>
       <SafeAreaView style={{marginTop:Platform.OS=="android"?StatusBar.currentHeight:0}}/>
-      <NavigationContainer>
-        <StackNav/>
-      </NavigationContainer>
+      <HomeScreen/>
     </View>
   );
 }
